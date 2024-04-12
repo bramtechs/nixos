3fbf8b0afe5e22d959ce593978c8bbddc8084172
bramtechs <bramtech@telenet.be>
2024-04-12 23:28:49 +0200

    write config for desktop

---
+++ configuration-desktop.nix
@@ -0,0 +1,14 @@
+{ config, lib, pkgs, callPackage, ... }:
+
+{
+    services.xserver.videoDrivers = ["nvidia"];
+
+     hardware.nvidia = {
+        modesetting.enable = true;
+        powerManagement.enable = false;
+        powerManagement.finegrained = false;
+        open = false;
+        nvidiaSettings = true;
+        package = config.boot.kernelPackages.nvidiaPackages.stable;
+    };
+}
--- configuration.nix
+++ configuration.nix
@@ -92,7 +92,7 @@
       hostName = "doomhowl"; # Define your hostname.
       networkmanager.enable = true;
       wireless.enable = false;
-      nameservers = [ "1.1.1.1" "1.0.0.1" ]
+      nameservers = [ "1.1.1.1" "1.0.0.1" ];
   };
 
   console = {
@@ -262,13 +262,5 @@
   systemd.automounts = [
     { where = "/mnt/nas"; automountConfig.TimeoutIdleSec = "1 min"; }
   ];
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "21.11"; # Did you read the comment?
 }
 
