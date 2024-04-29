33230b161768bb962f8830d7be135dce217329f5
bramtechs <bramtech@telenet.be>
2024-04-29 10:32:49 +0200

    ssh settings

--- configuration.nix
+++ configuration.nix
@@ -110,7 +110,13 @@
                 ];
 
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = false;
+      PermitRootLogin = "no";
+    };
+  };
 
   # extra hosts
   networking.extraHosts =
