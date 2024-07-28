a08adabb4ae0155e701d7d1f5423e2dcef56af70
bramtechs <bramtech@telenet.be>
2024-07-28 16:39:42 +0200

    try to do gitea proxy correctly

--- server/gitea.nix
+++ server/gitea.nix
@@ -1,10 +1,25 @@
-{ config, pkgs, ... }:
+{ ... }:
 
 {
   services.nginx = {
     enable = true;
     virtualHosts."doomhowl.local" = {
-      locations."/git" = { proxyPass = "http://localhost:3001/"; };
+      locations."~ ^/(git|v2)($|/)".extraConfig = ''
+        client_max_body_size 512M;
+
+        # make nginx use unescaped URI, keep "%2F" as-is, remove the "/git" sub-path prefix, pass "/v2" as-is.
+        rewrite ^ $request_uri;
+        rewrite ^(/git)?(/.*) $2 break;
+        proxy_pass http://127.0.0.1:3000$uri;
+
+        # other common HTTP headers, see the "Nginx" config section above
+        proxy_set_header Connection $http_connection;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+      '';
     };
   };
 
