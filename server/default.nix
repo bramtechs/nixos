b314598ee255400b9d077b3f0a4f3ca1b9938b52
bramtechs <bramtech@telenet.be>
2024-07-30 18:04:59 +0200

    more libvirt options

--- server/default.nix
+++ server/default.nix
@@ -48,7 +48,27 @@
     };
   };
 
-  virtualisation = { docker.enable = true; libvirtd.enable = true; };
+  virtualisation = {
+    docker.enable = true;
+    libvirtd = {
+      enable = true;
+      qemu = {
+        package = pkgs.qemu_kvm;
+        runAsRoot = true;
+        swtpm.enable = true;
+        ovmf = {
+          enable = true;
+          packages = [
+            (pkgs.OVMF.override {
+              secureBoot = true;
+              tpmSupport = true;
+            }).fd
+          ];
+        };
+      };
+    };
+    spiceUSBRedirection.enable = true;
+  };
 
   console = { keyMap = "us"; };
 
