3346bf319eb4b68eb6b47c807b66124869655102
bramtechs <bramtech@telenet.be>
2024-09-04 17:10:27 +0200

    remove unused virtualisation

--- setups/laptop-install.nix
+++ setups/laptop-install.nix
@@ -50,27 +50,6 @@
 
   # virtualization
   virtualisation = {
-    vmware = {
-      host.enable = true;
-    };
-    libvirtd = {
-      enable = true;
-      qemu = {
-        package = pkgs.qemu_kvm;
-        runAsRoot = true;
-        swtpm.enable = true;
-        ovmf = {
-          enable = true;
-          packages = [
-            (pkgs.OVMF.override {
-              secureBoot = true;
-              tpmSupport = true;
-            }).fd
-          ];
-        };
-      };
-    };
-    spiceUSBRedirection.enable = true;
     docker.enable = true;
   };
 
